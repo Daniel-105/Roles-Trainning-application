7bee66d7de4cfdcf1ea63cb600d7c71959fa02b0
DanielInfante <daniel.infante@netsigma.onmicrosoft.com>
2023-09-18 09:26:29 +0100

    Add project files.

---
+++ Roles-Trainning-application/Controllers/myPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roles_Trainning_application.Controllers
+{
+    public class myPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Roles-Trainning-application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Roles_Trainning_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
